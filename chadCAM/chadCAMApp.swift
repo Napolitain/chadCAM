9c094f616f06fa8ccf51dd3a45ab8007be8f1d00
Maxime Boucher <mxboucher@gmail.com>
2021-12-15 21:07:07 +0100

    Initial Commit

---
+++ chadCAM/chadCAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  chadCAMApp.swift
+//  chadCAM
+//
+//  Created by Maxime Boucher on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct chadCAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
